78ae7486f13196c8b634f8577258574c0e11e5ff
Deepak Rosario Pancras <rozartharigopla@gmail.com>
2023-06-20 03:11:54 -0700

    User Id changed
    Userid changed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 //import express from 'express';
 
 const app = express();
 
+passport.use(new GoogleStrategy());
+
 app.get('/', (req, res) => {
-    res.send({hi:'there'});
+    res.send({bye:'buddy'});
 });
 
 const PORT = process.env.PORT || 5000;
