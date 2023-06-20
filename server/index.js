41941973f17bd3301d4758701c8fa56e4fcc2d3c
Deepak Rosario Pancras <rozartharigopla@gmail.com>
2023-06-20 10:24:41 -0700

    Added google oauth
    Added google oauth

--- server/index.js
+++ server/index.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const keys = require('./config/keys');
 //import express from 'express';
 
 const app = express();
 
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        (accessToken, refreshToken, profile, done) =>{
+            console.log('TEST Access Token ####',accessToken);
+            console.log('TEST Refresh Token ####',refreshToken);
+            console.log('TEST Profile Token ####',profile);
+        }
+    )
+);
 
-app.get('/', (req, res) => {
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile','email']
+    })
+);
+
+
+app.get(
+    '/auth/google/callback', passport.authenticate('google')
+);
+/* app.get('/', (req, res) => {
     res.send({bye:'buddy'});
-});
+}); */
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
